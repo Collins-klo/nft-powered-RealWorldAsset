16a5a55f9b014eae327bacbae87bb150bae5e2be
Collins-klo <76812782+Collins-klo@users.noreply.github.com>
2025-04-01 21:55:28 +0300

    Add files via upload

---
+++ public/assets/icons/index.js
@@ -0,0 +1,32 @@
+import createNew from './createNew.svg';
+import home from './dashboard.svg';
+import estate from './estate.svg';
+import logo from './logo.svg';
+import logout from './logout.svg';
+import payment from './payment.svg';
+import profile from './profile.svg';
+import sun from './sun.svg';
+import bonds from './bonds.svg';
+import tagType from './type.svg';
+import search from './search.svg';
+import menu from './menu.svg';
+import money from './money.svg';
+import loader from './loader.svg';
+
+
+export {
+  tagType,
+  createNew,
+  home,
+  logo,
+  logout,
+  payment,
+  profile,
+  sun,
+  bonds,
+  search,
+  menu,
+  money,
+  loader,
+  estate,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      },
+      boxShadow: {
+        secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+      },
+    },
+  },
+  plugins: [],
+};
